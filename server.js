4e4847ce18d0b8c7ee440bee206947b5d8e7aa59
Simon Mai <maismin@gmail.com>
2019-10-24 20:54:03 -0400

    Update server.js to parse body

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ connectDB();
 
 const app = express();
 
+// Boder parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
