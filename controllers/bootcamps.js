5afe40ecd1f2f1a636cca023ef96462eb49f9994
Simon Mai <maismin@gmail.com>
2019-10-24 20:56:23 -0400

    Update getBootcamps route

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,15 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-const getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+const getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc    Get a single bootcamp
