3b2327d2956c2cec82130dcd508ffcd9298dc35f
Simon Mai <maismin@gmail.com>
2019-11-07 20:59:05 -0500

    Fix user registration to return JSON web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,7 +16,10 @@ const register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(201).json({ success: true });
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
 });
 
 module.exports = {
