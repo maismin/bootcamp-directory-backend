13ed0269e85384cb5cfeed07f064c6a234dfc922
Simon Mai <maismin@gmail.com>
2019-11-07 21:02:04 -0500

    Fix comment

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
-// @route   GET /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 const register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
