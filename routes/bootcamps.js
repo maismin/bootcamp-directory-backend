b87290ef8851e102bc5c677bce4296239994f382
Simon Mai <maismin@gmail.com>
2019-11-08 21:10:18 -0500

    Add API to get all reviews and get reviews for a specific bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,7 @@ const {
 
 // Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 // Authorization middlware
 const { protect, authorize } = require('../middleware/auth');
@@ -22,6 +23,7 @@ const router = express.Router();
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
