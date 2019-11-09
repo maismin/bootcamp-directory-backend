8415d7f92cc1b2220defde8e25fce0e991f9d6ab
Simon Mai <maismin@gmail.com>
2019-11-08 21:44:55 -0500

    Add API to add review for a specific bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const Review = require('../models/Review');
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 
 // Middlware
 const advancedResults = require('../middleware/advancedResults');
@@ -9,13 +9,16 @@ const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description',
-  }),
-  getReviews,
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getReviews,
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 
