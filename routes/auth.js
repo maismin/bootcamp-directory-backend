12975df3bb951db80a7ae8406504d05dbdad38e8
Simon Mai <maismin@gmail.com>
2019-11-08 19:40:28 -0500

    Add functionality to update user details such as name, email, and password

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const {
   register,
   login,
   getMe,
+  updateDetails,
+  updatePassword,
   forgotPassword,
   resetPassword,
 } = require('../controllers/auth');
@@ -15,6 +17,8 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
